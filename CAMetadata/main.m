db56f630a7ad7062167ae2bb1ac329b42974a909
rimalamir <amir.rimal@ekbana.info>
2021-08-24 14:12:39 +0545

    Initial Commit

---
+++ CAMetadata/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CAMetadata
+//
+//  Created by Amir Rimal on 24/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
