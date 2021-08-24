2d13cf658dffec4a8dab754a6658807d92860748
rimalamir <amir.rimal@ekbana.info>
2021-08-24 15:51:27 +0545

    First commit

--- CAMetadata/main.m
+++ CAMetadata/main.m
@@ -6,11 +6,41 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreAudio/CoreAudio.h>
+#import <AudioToolbox/AudioFile.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        if (argc < 2) {
+            printf("Usage: CAMetadata /full/path/to/audiofile\n");
+            return  -1;
+        }
+        
+        NSString *audioFilePath = [[NSString stringWithUTF8String:argv[1]] stringByExpandingTildeInPath];
+        NSURL *audioURL = [NSURL fileURLWithPath:audioFilePath];
+        
+        AudioFileID audioFile;
+        OSStatus theErr = noErr;
+        
+        theErr = AudioFileOpenURL((__bridge CFURLRef)audioURL, kAudioFileReadPermission, 0, &audioFile);
+        assert(theErr == noErr);
+        
+        UInt32 dictionarySize = 0;
+        theErr = AudioFileGetPropertyInfo(audioFile, kAudioFilePropertyInfoDictionary, &dictionarySize, 0);
+        
+        assert(theErr == noErr);
+        
+        CFDictionaryRef dictionary;
+        theErr = AudioFileGetProperty(audioFile, kAudioFilePropertyInfoDictionary, &dictionarySize, &dictionary);
+        
+        assert(theErr == noErr);
+        NSLog(@"dictionary: %@", dictionary);
+        CFRelease(dictionary);
+        
+        theErr = AudioFileClose(audioFile);
+        
+        assert(theErr == noErr);
     }
     return 0;
 }
